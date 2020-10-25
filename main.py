d102692ba7269e8acee6028b41041536aaf3ede0
tndevDE <tobi.nieberle@gmail.com>
2020-10-25 10:50:52 +0100

    add GUI

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from tkinter import *
 import ctypes
 
+
+fenster = Tk()
+fenster.title("no_screensaver")
+info_label = Label(fenster, text="Programm geöffnet lassen, um no_screensaver zu verwenden.")
+info_label.pack()
 ctypes.windll.kernel32.SetThreadExecutionState(0x80000002)
-input('{Press enter to exit}')
+fenster.mainloop()
 ctypes.windll.kernel32.SetThreadExecutionState(0x80000000)
